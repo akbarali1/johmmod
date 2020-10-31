16728b6373cfaa10a1a0cec8b90b3f2ae2e09832
akbar9705 <39323182+Akbar9705@users.noreply.github.com>
2020-10-31 18:22:38 +0500

    qo`shildi

--- modules/images/index.php
+++ modules/images/index.php
@@ -4,6 +4,7 @@ defined('_IN_JOHNCMS') || die('Error: restricted access');
 
 // Инициализируем шаблонизатор
 $view = di(Johncms\System\View\Render::class);
+$user = di(Johncms\Users\User::class);
 
 // Инициализируем хлебные крошки (цепочка навигации вверху всех страниц)
 $nav_chain = di(Johncms\NavChain::class);
@@ -14,6 +15,24 @@ $view->addFolder('images', __DIR__ . '/templates/');
 // Добавляем ссылку Контакты в хлебные крошки
 $nav_chain->add('Images', '/images/');
 $db = di(PDO::class);
+
+ if ($user->isValid()){
+  
+ }else{ ?>
+ <!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>только тем, кто зарегистрирован</title>
+</head>
+<body>
+   <p>только тем, кто зарегистрирован</p> 
+</body>
+</html>
+  <?php
+  exit();
+ }
 
 $req = $db->query('SELECT * FROM `images_akb` ORDER by `time` DESC');
 while ($row = $req->fetch()) {
